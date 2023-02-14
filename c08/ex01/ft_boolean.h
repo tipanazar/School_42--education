db7947f1e4a0c277cd451455808d061c842093b3
Nazar <83466233+tipanazar@users.noreply.github.com>
2023-02-14 19:41:21 +0000

    Add files via upload

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkarpeko <nkarpeko@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 19:23:46 by nkarpeko          #+#    #+#             */
+/*   Updated: 2023/02/14 19:33:01 by nkarpeko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int	t_bool;
+
+# define SUCCESS 0
+# define TRUE 1
+# define FALSE 0
+# define EVEN(n) n % 2 == 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+#endif
