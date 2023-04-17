abb4b95cae5430ebac8acc3c1cf06bd4bedb9fe7
Nazar Karpeko <nkarpeko@c2r1s6.42lisboa.com>
2023-04-17 14:30:13 +0100

    strjoin is done

--- Main_Core/libft/libft.h
+++ Main_Core/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nkarpeko <nkarpeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:21:48 by nkarpeko          #+#    #+#             */
-/*   Updated: 2023/04/17 13:13:29 by nkarpeko         ###   ########.fr       */
+/*   Updated: 2023/04/17 14:01:32 by nkarpeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char			*ft_strrchr(const char *s, int c);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
 
 #endif
