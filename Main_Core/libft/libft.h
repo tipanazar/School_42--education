129690d673b751bf27952db4708a1958e69769d5
Nazar Karpeko <nkarpeko@c2r1s6.42lisboa.com>
2023-04-17 19:48:41 +0100

    striteri is done

--- Main_Core/libft/libft.h
+++ Main_Core/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nkarpeko <nkarpeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:21:48 by nkarpeko          #+#    #+#             */
-/*   Updated: 2023/04/17 19:41:44 by nkarpeko         ###   ########.fr       */
+/*   Updated: 2023/04/17 19:45:47 by nkarpeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ char			*ft_strtrim(char const *s1, char const *set);
 char			**ft_split(char const *s, char c);
 char			*ft_itoa(int n);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void			ft_striteri(char *s, void (*f)(unsigned int, char *));
 
 #endif
