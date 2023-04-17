54091df648b0dedd21228ba18ae215e382f5df87
Nazar Karpeko <nkarpeko@c2r1s6.42lisboa.com>
2023-04-17 20:07:37 +0100

    putstr_fd is done

--- Main_Core/libft/libft.h
+++ Main_Core/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: nkarpeko <nkarpeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 13:21:48 by nkarpeko          #+#    #+#             */
-/*   Updated: 2023/04/17 20:03:15 by nkarpeko         ###   ########.fr       */
+/*   Updated: 2023/04/17 20:04:18 by nkarpeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,6 @@ char			*ft_itoa(int n);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void			ft_striteri(char *s, void (*f)(unsigned int, char *));
 void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
 
 #endif
