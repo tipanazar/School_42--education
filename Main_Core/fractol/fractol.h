dc2b4fc8e7db5e52a98ab29f3d07944c082cc2db
tipanazar <nkarpeko@student.42lisboa.com>
2023-07-02 19:41:56 +0100

    render flag

--- Main_Core/fractol/fractol.h
+++ Main_Core/fractol/fractol.h
@@ -6,7 +6,7 @@
 /*   By: tipanazar <tipanazar@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 17:04:47 by nkarpeko          #+#    #+#             */
-/*   Updated: 2023/07/02 17:33:16 by tipanazar        ###   ########.fr       */
+/*   Updated: 2023/07/02 17:58:02 by tipanazar        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define FRACTOL_H
 
 #include "./libft/libft.h"
-
+#include "./mlx_linux/mlx.h"
+#include "./mlx_linux/mlx_int.h"
 
 #endif
--- Main_Core/fractol/main.c
+++ Main_Core/fractol/main.c
@@ -6,13 +6,35 @@
 /*   By: tipanazar <tipanazar@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 17:04:49 by nkarpeko          #+#    #+#             */
-/*   Updated: 2023/07/02 17:32:03 by tipanazar        ###   ########.fr       */
+/*   Updated: 2023/07/02 19:39:49 by tipanazar        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int main(void)
+int	main(void)
 {
-    ft_printf("Test\n");
+	int f_idx = -1;
+	int s_idx = 0;
+	int blue = 0x0000ff;
+	int yellow = 0xffff00;
+	void *mlx_window;
+	void *mlx_connection = mlx_init();
+	mlx_window = mlx_new_window(mlx_connection, 800, 600, "Glory to Ukraine!");
+	while (++f_idx <= 700)
+	{
+		while (++s_idx <= 250)
+			mlx_pixel_put(mlx_connection, mlx_window, 50 + f_idx, 50 + s_idx,
+					blue);
+		s_idx = 0;
+	}
+	f_idx = -1;
+	while (++f_idx <= 700)
+	{
+		while (++s_idx <= 250)
+			mlx_pixel_put(mlx_connection, mlx_window, 50 + f_idx, 300 + s_idx,
+					yellow);
+		s_idx = 0;
+	}
+	mlx_loop(mlx_connection);
 }
