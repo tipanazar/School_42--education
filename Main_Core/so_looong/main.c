71564b4148044353cb215c0292c37ca2efd0614b
tipanazar <karpeko76@gmail.com>
2023-07-12 18:23:04 +0100

    upd

--- Main_Core/so_looong/main.c
+++ Main_Core/so_looong/main.c
@@ -6,17 +6,44 @@
 /*   By: nkarpeko <nkarpeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 17:04:49 by nkarpeko          #+#    #+#             */
-/*   Updated: 2023/07/11 17:55:07 by nkarpeko         ###   ########.fr       */
+/*   Updated: 2023/07/12 18:22:51 by nkarpeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	ft_define_vars(t_vars *vars)
+{
+	vars->mlx = mlx_init();
+	vars->win = mlx_new_window(vars->mlx, WIDTH, HEIGHT, "Window");
+	vars->idx = -1;
+	vars->color = 0xffff00;
+	vars->gap = 100;
+}
 
+void	ft_loop(t_vars *vars)
+{
+	(void) vars;
+}
+
+
+
+void	ft_render(t_vars *vars)
+{
+	int pos = 0;
+	vars->img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
+	vars->addr = mlx_get_data_addr(vars->img, &vars->bits_per_pixel,
+			&vars->line_length, &vars->endian);
+	// ft_loop(vars);
+
+    vars->img = mlx_xpm_file_to_image(vars->mlx, "./files/textures/w221.xpm", &pos, &pos);
+
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->img, 0, 0);
+}
 
 int	main(void)
 {
-	t_vars vars;
+	t_vars	vars;
 
 	ft_define_vars(&vars);
 	ft_render(&vars);
@@ -25,3 +52,4 @@ int	main(void)
 	mlx_mouse_hook(vars.win, ft_mouse_handler, &vars);
 	mlx_loop(vars.mlx);
 }
+
--- Main_Core/so_looong/so_long.h
+++ Main_Core/so_looong/so_long.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.h                                          :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nkarpeko <nkarpeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 17:04:47 by nkarpeko          #+#    #+#             */
-/*   Updated: 2023/07/11 17:53:35 by nkarpeko         ###   ########.fr       */
+/*   Updated: 2023/07/12 15:38:16 by nkarpeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 
 # include "./libft/libft.h"
 # include "./mlx_linux/mlx.h"
-#include <stdlib.h>
-#include <unistd.h>
 # include <math.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # define WIDTH 1500
 # define HEIGHT 1500
 
 // #define WIDTH 800
 // #define HEIGHT 600
-#define MAX_ITERATIONS 1000
-
+# define MAX_ITERATIONS 1000
 
 typedef struct s_vars
 {
@@ -40,9 +39,27 @@ typedef struct s_vars
 	int		color;
 	int		gap;
 	//
-    double real;
-    double imag;
+	double	real;
+	double	imag;
 }			t_vars;
+
+typedef struct s_game
+{
+	int		height;
+	int		width;
+	int		player_position_x;
+	int		player_position_y;
+	int		score;
+	int		current_score;
+	char	**mapdata;
+	void	*wall;
+	void	*exit;
+	void	*items;
+	void	*player;
+	int		enemy_position_x;
+	int		enemy_position_y;
+	int		game_over;
+}			t_game;
 
 void		my_mlx_pixel_put(t_vars *data, int x, int y, int color);
 int			ft_mouse_handler(int button, int x, int y, t_vars *vars);
@@ -50,7 +67,6 @@ int			ft_handle_key(int keycode, t_vars *vars);
 int			ft_close(t_vars *vars);
 void		ft_render(t_vars *vars);
 //
-void	hsv_to_rgb(double h, double s, double v, double rgb[3]);
-
+void		hsv_to_rgb(double h, double s, double v, double rgb[3]);
 
 #endif
