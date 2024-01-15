3ca3669d662b3f2f3851b36f92e9161b4fa80b8f
Nazar <83466233+tipanazar@users.noreply.github.com>
2024-01-15 14:37:19 +0000

    upd

--- Main_Core/philosophers/main.c
+++ Main_Core/philosophers/main.c
@@ -1,17 +1,12 @@
-
 #include "./philo.h"
 
-typedef struct s_args
+void data_validator(int argc, char **argv, t_data *args)
 {
-    int num_of_philo;                     // number of forks as well
-    int time_to_die;                      //* If a philosopher didn’t start eating time_to_die milliseconds since the beginning of their last meal or the beginning of the simulation, they die.
-    int time_to_eat;                      //* The time it takes for a philosopher to eat. During that time, they will need to hold two forks.
-    int time_to_sleep;                    //* The time a philosopher will spend sleeping.
-    int num_of_times_each_philo_must_eat; //* If all philosophers have eaten at least number_of_times_each_philosopher_must_eat times, the simulation stops. If not specified, the simulation stops when a philosopher dies.
-} t_args;
-
-void data_validator(char **argv, t_args *args)
-{
+    if (argc < 5 || argc > 6)
+    {
+        printf("Error: Wrong number of arguments\n");
+        exit(1);
+    }
     args->num_of_philo = ft_atoi(argv[1]);
     args->time_to_die = ft_atoi(argv[2]);
     args->time_to_eat = ft_atoi(argv[3]);
@@ -27,14 +22,43 @@ void data_validator(char **argv, t_args *args)
     }
 }
 
+// void *routine(void *arg)
+// {
+//     t_data *philo;
+
+//     philo = (t_data *)arg;
+//     while (1)
+//     {
+//         // think
+//         // take forks
+//         // eat
+//         // put forks
+//         // sleep
+//     }
+// }
+
+void init_philosophers()
+{
+    
+}
+
 int main(int argc, char **argv)
 {
-    t_args args;
+    t_data args;
 
-    if (argc < 5 || argc > 6)
-    {
-        printf("Error: Wrong number of arguments\n");
-        exit(1);
-    }
-    data_validator(argv, &args);
+    data_validator(argc, argv, &args);
+    args.num_of_philo = argv[1];
+    args.time_to_die = argv[2];
+    args.time_to_eat = argv[3];
+    args.time_to_sleep = argv[4];
+    if (argv[5])
+        args.num_of_times_each_philo_must_eat = argv[5];
+    else 
+        args.num_of_times_each_philo_must_eat = -1;    
 }
+
+// int num_of_philo;                     //* number of forks as well
+// int time_to_die;                      //* If a philosopher didn’t start eating time_to_die milliseconds since the beginning of their last meal or the beginning of the simulation, they die.
+// int time_to_eat;                      //* The time it takes for a philosopher to eat. During that time, they will need to hold two forks.
+// int time_to_sleep;                    //* The time a philosopher will spend sleeping.
+// int num_of_times_each_philo_must_eat; //* If all philosophers have eaten at least number_of_times_each_philosopher_must_eat times, the simulation stops. If not specified, the simulation stops when a philosopher dies.
--- Main_Core/philosophers/philo.h
+++ Main_Core/philosophers/philo.h
@@ -6,6 +6,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct s_data
+{
+    int num_of_philo;                     //* number of forks as well
+    int time_to_die;                      //* If a philosopher didn’t start eating time_to_die milliseconds since the beginning of their last meal or the beginning of the simulation, they die.
+    int time_to_eat;                      //* The time it takes for a philosopher to eat. During that time, they will need to hold two forks.
+    int time_to_sleep;                    //* The time a philosopher will spend sleeping.
+    int num_of_times_each_philo_must_eat; //* If all philosophers have eaten at least number_of_times_each_philosopher_must_eat times, the simulation stops. If not specified, the simulation stops when a philosopher dies.
+} t_data;
+
+
+
+// typedef struct s_philo
+// {
+// 	struct s_data	*data;
+// 	pthread_t       t1;
+// 	int             id;
+// 	int             eat_cont;
+// 	int             status;
+// 	int             eating;
+// 	uint64_t        time_to_die;
+// 	pthread_mutex_t	lock;
+// 	pthread_mutex_t	*r_fork;
+// 	pthread_mutex_t	*l_fork;
+// } t_philo;
+
+// typedef struct s_data
+// {
+// 	pthread_t       *tid;
+// 	int             philo_num;
+// 	int             meals_nb;
+// 	int             dead;
+// 	int             finished;
+// 	t_philo         *philos;
+// 	u_int64_t       death_time;
+// 	u_int64_t       eat_time;
+// 	u_int64_t       sleep_time;
+// 	u_int64_t       start_time;
+// 	pthread_mutex_t *forks;
+// 	pthread_mutex_t lock;
+// 	pthread_mutex_t write;
+// } t_data;
+
+
 int ft_atoi(char *str);
 
 #endif
