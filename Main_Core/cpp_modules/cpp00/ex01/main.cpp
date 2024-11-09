db528f926f0871fb9ceb9f20e778d9ef634c3a28
tipanazar <karpeko76@gmail.com>
2024-11-09 20:15:21 +0000

    upd for intels

--- Main_Core/cpp_modules/cpp00/ex01/main.cpp
+++ Main_Core/cpp_modules/cpp00/ex01/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <cctype>
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
+
+bool isValidInteger(const std::string &str)
+{
+
+    for (size_t i = 0; i < str.size(); ++i)
+    {
+        if (!std::isdigit(str[i]))
+            return false;
+    }
+
+    return true;
+}
 
 void ft_process_add(PhoneBook &phoneBook)
 {
@@ -38,6 +53,12 @@ void ft_process_search(PhoneBook &phoneBook)
 {
     std::string index;
 
+    if (phoneBook.get_contacts_number() == 0)
+    {
+        std::cout << "Phone Book is empty, nothing to search!" << std::endl;
+        return;
+    }
+
     phoneBook.ft_show_list();
     while (1)
     {
@@ -53,17 +74,20 @@ void ft_process_search(PhoneBook &phoneBook)
         }
         try
         {
-            int idx = std::stoi(index);
-            if (idx < 0 || idx >= phoneBook.get_contacts_number())
+            if (!isValidInteger(index))
+                throw std::invalid_argument("Invalid input: Not a valid integer.");
+            int idx = atoi(index.c_str());
+            if (idx >= phoneBook.get_contacts_number())
             {
                 std::cout << "Error: Invalid index" << std::endl;
                 continue;
             }
+            std::cout << std::endl;
             phoneBook.printContact(idx);
         }
         catch (const std::invalid_argument &e)
         {
-            std::cout << "Error: Please enter a valid integer index or type EXIT" << std::endl;
+            std::cout << e.what() << std::endl;
         }
         catch (const std::out_of_range &e)
         {
